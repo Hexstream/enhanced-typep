17ebbeaedd305cb4798dfbadb8c47b7f312a20e5
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2020-10-17 14:23:32 -0400

    Pass compile time environment to CONSTANTP.
    (Else we might miss an opportunity to inline the constant in obscure cases.)

--- main.lisp
+++ main.lisp
@@ -11,9 +11,9 @@
       (lambda (object)
         (cl:typep object object-or-type))))
 
-(defun %typep-lambda (type)
+(defun %typep-lambda (type &optional compile-time-environment)
   (let* ((object-var (gensym (string '#:object)))
-         (type-var (unless (constantp type)
+         (type-var (unless (constantp type compile-time-environment)
                      (gensym (string '#:type))))
          (main `(lambda (,object-var)
                   (cl:typep ,object-var ,(or type-var type)))))
@@ -22,8 +22,9 @@
            ,main)
         main)))
 
-(define-compiler-macro typep (object-or-type &optional (type nil type-supplied-p) (environment nil environment-supplied-p))
+(define-compiler-macro typep (object-or-type &optional (type nil type-supplied-p) (environment nil environment-supplied-p)
+                                             &environment compile-time-environment)
   (if type-supplied-p
       `(cl:typep ,object-or-type ,type ,@(when environment-supplied-p
                                            (list environment)))
-      (%typep-lambda object-or-type)))
+      (%typep-lambda object-or-type compile-time-environment)))
